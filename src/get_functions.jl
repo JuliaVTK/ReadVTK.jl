d169a570a57da20fe17e11cb75fdd6dd35c75c58
Jorge Pérez Zerpa <42485529+jorgepz@users.noreply.github.com>
2022-06-09 05:41:13 -0300

    Update src/get_functions.jl @ranocha
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/get_functions.jl
+++ src/get_functions.jl
@@ -5,8 +5,7 @@ get_origin(vtk_file)
 Retrieve the origin of a VTK regular structured grid file.
 
 """
-
-function get_origin( vtk_file )
+function get_origin(vtk_file)
 
   root = LightXML.root( vtk_file.xml_file )
 
