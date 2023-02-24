81f52f9b92d7707c1d4e4103c62573587ee6064b
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2023-02-24 15:58:21 +0100

    Update src/ReadVTK.jl
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>

--- src/ReadVTK.jl
+++ src/ReadVTK.jl
@@ -522,13 +522,11 @@ function get_points(vtk_file)
 end
 
 """
-  get_coordinates(vtk_file; x_string="x",y_string="y",z_string="z")
+    get_coordinates(vtk_file; x_string="x", y_string="y", z_string="z")
 
-Retrieve VTK coordinate vectors in each direction as 1D vectors for a RectilinearGrid file
+Retrieve VTK coordinate vectors in each direction as a tuple of 1D vectors for a RectilinearGrid file.
 
-The points are given as 1D vectors. Note that in VTK, points are always stored
-three-dimensional, even for 1D or 2D files, so you will always retrieve 3 vectors.
-The 
+Note that in VTK, points are always stored three-dimensional, even for 1D or 2D files, so you will always retrieve a tuple with three vectors.
 
 See also: [`get_cells`](@ref)
 """
