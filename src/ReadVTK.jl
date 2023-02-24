e89d8c00c531794788e07e06053c1c361add337b
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2023-02-24 15:54:32 +0100

    Update src/ReadVTK.jl
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>

--- src/ReadVTK.jl
+++ src/ReadVTK.jl
@@ -453,12 +453,11 @@ end
 
 
 """
-    get\\_data\\_reshaped(data_array::VTKDataArray; cell_data=false)
+    get_data_reshaped(data_array::VTKDataArray; cell_data=false)
 
-Retrieve actual data from a `VTKDataArray` and reshapes them as 1D, 2D or 3D arrays, in case we deal with structured grids.
+Retrieve actual data from a `VTKDataArray` and reshape it as 1D, 2D, or 3D arrays, in case we deal with structured grids.
 Note that vectors or tensors will have their components stored in the first dimension of the array. 
-As there is no way to automatically tell from the VTK file format whether it is a tensor, the user has to reshape this accordingly.
-
+As there is no way to automatically tell from the VTK file format whether it is a tensor, the user has to reshape this manually.
 """
 function get_data_reshaped(data_array::VTKDataArray{T,N}; cell_data=false) where {T,N}
 
