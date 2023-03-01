52a603580aa4ceffd658cd1931f32511009fc06d
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2023-03-01 23:04:40 +0100

    Update src/get_functions.jl
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>

--- src/get_functions.jl
+++ src/get_functions.jl
@@ -23,7 +23,7 @@ end
 
 
 """
-    get_origin(vtk_file::PVTKFile)
+    get_origin(pvtk_file::PVTKFile)
 
 Retrieve the vector of coordinates of the origin of a uniform grid from the given [`PVTKFile`](@ref) file.
 
