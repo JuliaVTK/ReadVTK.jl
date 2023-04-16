3fc7a240e85ec5ba3618be7fb3f622e51bf80e0f
Funnyspell22 <mjmw94@gmail.com>
2023-04-16 16:26:22 -0400

    @ref fix for MeshCell for Documenter

--- src/ReadVTK.jl
+++ src/ReadVTK.jl
@@ -1138,7 +1138,7 @@ end
 
 Convert a `VTKCells` object, which holds raw point and connectivity data for a number of cells, to
 a vector of `MeshCell` objects. The latter can, e.g., be passed to the WriteVTK.jl package for writing new VTK files.
-See also: [`VTKCells`](@ref), [`MeshCell`](@ref)
+See also: [`VTKCells`](@ref), [`VTKBase.MeshCell`](@ref)
 """
 function to_meshcells(cells::VTKCells)
 
