c229dce68674d992586dac4dfb271313ddc20a92
Hendrik Ranocha <mail@ranocha.de>
2023-04-20 10:18:43 +0200

    Revert "format src"
    This reverts commit c188741c60619d1b9d4fc768b10349f8d9ece434.

--- src/get_functions.jl
+++ src/get_functions.jl
@@ -12,15 +12,15 @@ function get_origin(vtk_file::VTKFile)
   whole_extent = get_whole_extent(vtk_file)
 
   # obtain the origin
-  origin = parse.(Float64,
-                  split(attribute(dataset_element, "Origin", required = true), ' '))
+  origin = parse.(Float64, split(attribute(dataset_element, "Origin", required=true), ' '))
 
-  if (whole_extent[5:6] == [0, 0]) && (origin[3] == 0.0)
+  if (whole_extent[5:6] == [0,0]) && (origin[3] == 0.0)
     deleteat!(origin, 3)
   end
 
   return origin
 end
+
 
 """
     get_origin(pvtk_file::PVTKFile)
@@ -43,8 +43,7 @@ function get_spacing(vtk_file::VTKFile)
   dataset_element = get_imagedata_dataset(vtk_file)
 
   # obtain the spacing
-  spacing = parse.(Float64,
-                   split(attribute(dataset_element, "Spacing", required = true), ' '))
+  spacing = parse.(Float64, split(attribute(dataset_element, "Spacing", required=true), ' '))
 
   if length(get_origin(vtk_file)) == 2
     deleteat!(spacing, 3)
@@ -62,6 +61,7 @@ See also: [`PVTKFile`](@ref)
 """
 get_spacing(pvtk_file::PVTKFile) = get_spacing(VTKFile(pvtk_file.vtk_filenames[1]))
 
+
 """
     get_whole_extent(vtk_file::VTKFile)
 
@@ -74,9 +74,7 @@ function get_whole_extent(vtk_file::VTKFile)
   dataset_element = get_imagedata_dataset(vtk_file)
 
   # obtain extent
-  whole_extent = parse.(Int,
-                        split(attribute(dataset_element, "WholeExtent", required = true),
-                              ' '))
+  whole_extent = parse.(Int, split(attribute(dataset_element, "WholeExtent", required=true), ' '))
 
   return whole_extent
 end
@@ -88,9 +86,8 @@ Retrieve a vector with the `WholeExtent` 6-entry vector from the uniform grid [`
 
 See also: [`PVTKFile`](@ref)
 """
-function get_whole_extent(pvtk_file::PVTKFile)
-  get_whole_extent(VTKFile(pvtk_file.vtk_filenames[1]))
-end
+get_whole_extent(pvtk_file::PVTKFile) = get_whole_extent(VTKFile(pvtk_file.vtk_filenames[1]))
+
 
 """
     get_imagedata_dataset(vtk_file::VTKFile)
@@ -100,17 +97,18 @@ Retrieve ImageData dataset from the given [`VTKFile`](@ref) file.
 See also: [`VTKFile`](@ref)
 """
 function get_imagedata_dataset(vtk_file::VTKFile)
-  # check imagedata
-  if vtk_file.file_type != "ImageData"
-    error("the file_type must be ImageData.")
-  end
+    # check imagedata
+    if vtk_file.file_type != "ImageData"
+      error("the file_type must be ImageData.")
+    end
 
-  # open the file and locate the ImageData section
-  root = LightXML.root(vtk_file.xml_file)
-  dataset_element = root["ImageData"][1]
+    # open the file and locate the ImageData section
+    root = LightXML.root(vtk_file.xml_file)
+    dataset_element = root["ImageData"][1]
 
-  return dataset_element
+    return dataset_element
 end
+
 
 """
     get_imagedata_dataset(pvtk_file::PVTKFile)
