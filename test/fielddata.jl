14520c6f9f78bf64a32679aa1abdcf90139d8c45
Tuukka Verho <tuukka.verho@vtt.fi>
2024-10-27 18:42:24 +0200

    Format field data test

--- test/fielddata.jl
+++ test/fielddata.jl
@@ -1,18 +1,18 @@
 # Create some cells
 points = rand(3, 5)
 cells = [
-     MeshCell(VTKCellTypes.VTK_TRIANGLE, [1, 4, 2]),
-     MeshCell(VTKCellTypes.VTK_QUAD, [2, 4, 3, 5]),
-   ]
+  MeshCell(VTKCellTypes.VTK_TRIANGLE, [1, 4, 2]),
+  MeshCell(VTKCellTypes.VTK_QUAD, [2, 4, 3, 5])
+]
 
-scalar = 5.
-vector = [1., 2., 3.]
+scalar = 5.0
+vector = [1.0, 2.0, 3.0]
 
 path = joinpath(TEST_EXAMPLES_DIR, "fields")
 
-vtk_grid(path, points, cells, append=false) do vtk
-    vtk["scalar"] = scalar
-    vtk["vector"] = vector
+vtk_grid(path, points, cells, append = false) do vtk
+  vtk["scalar"] = scalar
+  return vtk["vector"] = vector
 end
 
 vtk_read = VTKFile(path * ".vtu")
